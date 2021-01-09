6cdd4b376c22213e0c4c29d98b8f33ab674ae66b
Zach R <69822582+SmallPepperZ@users.noreply.github.com>
2021-01-09 13:36:56 -0800

    fix error with no argument for simonsays

--- bot.py
+++ bot.py
@@ -146,30 +146,33 @@ async def repeatembed(ctx, *, content:str):
 
 @bot.command(aliases=['repeat'])
 #@bot.check(commands.is_owner())
-async def simonsays(ctx, *, content:str):
-	if ctx.message.author.id != 545463550802395146: 
-		m1 = ":| You can't push me around like that"
-		m2 = "You literally typed 11 extra characters to try and get me to do something for you"
-		m3 = "Um, no thanks"
-		m4 = "I'd reallly rather not say that"
-		m5 = "Just say it yourself"
-		m6 = "C'mon, just... just remove '%simonsays' and it works"
-		m7 = "I am not your speech bot"
-		m8 = "You aren't paying me, so no thanks"
-		m9 = "I don't work for free"
-		m10 = "Make your own simonsays bot"
-		try:
-			m11 = str(ctx.author.nickname)+" asked me politely to say "+content
-		except:
-			m11 = "somebody forgot to add a message"
-		m12 = "I've always wanted to be a simon"
-		msg = random.choice([m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12])
-		await ctx.reply(str(msg))
-		print(content+' echo attempted by '+str(ctx.author))
+async def simonsays(ctx, *, content:str=None):
+	if not content:
+		await ctx.reply("Give me something to say!")
 	else:
-		await ctx.message.delete()
-		await ctx.send(content)
-		print(content+' echoed by '+str(ctx.author))
+		if ctx.message.author.id != 545463550802395146: 
+			m1 = ":| You can't push me around like that"
+			m2 = "You literally typed 11 extra characters to try and get me to do something for you"
+			m3 = "Um, no thanks"
+			m4 = "I'd reallly rather not say that"
+			m5 = "Just say it yourself"
+			m6 = "C'mon, just... just remove '%simonsays' and it works"
+			m7 = "I am not your speech bot"
+			m8 = "You aren't paying me, so no thanks"
+			m9 = "I don't work for free"
+			m10 = "Make your own simonsays bot"
+			try:
+				m11 = str(ctx.author.nickname)+" asked me politely to say "+content
+			except:
+				m11 = "somebody forgot to add a message"
+			m12 = "I've always wanted to be a simon"
+			msg = random.choice([m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12])
+			await ctx.reply(str(msg))
+			print(content+' echo attempted by '+str(ctx.author))
+		else:
+			await ctx.message.delete()
+			await ctx.send(content)
+			print(content+' echoed by '+str(ctx.author))
 
 @bot.command()
 async def delete(ctx, messageid):
