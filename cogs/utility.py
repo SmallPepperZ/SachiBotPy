cfdbbf7a4ed770f64a1f4651277176de98369837
Zach R <69822582+SmallPepperZ@users.noreply.github.com>
2021-01-21 18:28:08 -0800

    make whois command work with any userid/mention

--- cogs/utility.py
+++ cogs/utility.py
@@ -18,7 +18,7 @@ token = configjson["token"]
 
 
 
-class UtilityCog(commands.Cog):
+class UtilityCog(commands.Cog, name="Utility"):
 	def __init__(self, bot):
 		self.bot = bot
 
@@ -46,26 +46,47 @@ class UtilityCog(commands.Cog):
 		print('Pinged by '+str(ctx.author))
 	
 	@commands.command(aliases=['userinfo'])
-	async def whois(self,ctx):
-		user = ctx.message.mentions[0]
+	async def whois(self,ctx, userid):
+		try:
+			userid = ctx.message.mentions[0].id
+		except:
+			userid = int(userid)
+		isguildmember = ctx.guild.get_member(userid) != None
+		if isguildmember:
+			user = ctx.guild.get_member(userid)
+			isadmin = user.guild_permissions.administrator
+			nickname = user.display_name
+			joindate = user.joined_at
+			isowner = ctx.guild.owner.id == user.id
+			def embedsec1(embed):
+				embed.add_field(name="Is the owner?", value=isowner, inline='true')
+				embed.add_field(name="Is an admin?", value=isadmin, inline='true')
+				embed.add_field(name="Nickname", value=nickname, inline="True")
+			def embedsec2(embed):
+				embed.add_field(name="Join Date", value=joindate, inline='true')
+		else:
+			user = self.bot.get_user(userid)
+			def embedsec1(embed):
+				return
+			def embedsec2(embed):
+				return
 		isbot = user.bot
 		avatar = user.avatar_url
-		try:
-			isowner = ctx.guild.owner.id == user.id
-		except AttributeError:
-			isowner = "N/A"
 		createdate = user.created_at
-		nickname = user.display_name
-		joindate = user.joined_at
+		mention = user.mention
+		userid = user.id
 		username = user.name+"#"+user.discriminator
 		color = user.color
-		embed = discord.Embed(color=color,title=username, description='d')
+		embed = discord.Embed(color=color,title=username)
+		embed.set_footer(text=f"Request by {ctx.author}", icon_url= ctx.author.avatar_url)
 		embed.set_image(url=avatar)
-		embed.add_field(name="Is a bot?", value=isbot, inline='false')
-		embed.add_field(name="Is the owner?", value=isowner, inline='true')
-		embed.add_field(name="Creation Date", value=createdate, inline='false')
-		embed.add_field(name="Join Date", value=joindate, inline='true')
-		await ctx.reply(embed=embed)
+		embed.add_field(name="Is a bot?", value=isbot, inline='true')
+		embedsec1(embed)
+		embed.add_field(name="Mention", value=mention, inline="False")
+		embed.add_field(name="ID", value=f'`{user.id}`', inline="False")
+		embed.add_field(name="Account Creation Date", value=createdate, inline='false')
+		embedsec2(embed)
+		await ctx.send(embed=embed)
 		#ctx.guild.get_member(user)
 
 
