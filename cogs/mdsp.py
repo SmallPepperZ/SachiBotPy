139780fc1204d07396990ef1ca4ce6ea16a8fdb5
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-01-24 19:24:57 -0800

    Add invite status modification commands

--- cogs/mdsp.py
+++ cogs/mdsp.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 import json
+from discord.ext.commands import MessageConverter
 from mee6_py_api import API
-
 
 
 
@@ -15,6 +15,9 @@ configjson = json.loads(configfile)
 embedcolor = int(configjson["embedcolor"], 16)
 
 mee6API = API(302094807046684672)
+
+invitechannelid = 802245295291236442
+invitechannellimit = 1
 prefix = configjson["prefix"]
 #endregion
 
@@ -24,7 +27,68 @@ def addfield(embed, key:str, value:str):
 	embed.__setattr__("description", f'{embed.description}\n**{key}:** {value}')
 def addrow(embed, value:str):
 	embed.__setattr__("description", f'{embed.description}\n{value}')
+def listlines(messagecontents):
+	splitmessage = messagecontents.description.splitlines()
+	l1 = splitmessage[0]
+	l2 = splitmessage[1]
+	l3 = splitmessage[2]
+	l4 = splitmessage[3]
+	l5 = splitmessage[4]
+	l6 = splitmessage[5]
+	return l1, l2, l3, l4, l5, l6
 
+async def updateinvitestatus(ctx, infomsg: discord.Message, messages, user: discord.User, invitechannel: discord.TextChannel, action:str):
+	terms = {
+		"approve":{
+			"word1": "Approving",
+			"word2": "<:Allowed:786997173845622824> - Approved",
+			"word3": "approved",
+			"color": 0x00FF00
+		},
+		"deny":{
+			"word1": "Denying",
+			"word2": "<:Denied:786997173820588073> - Denied",
+			"word3": "denied",
+			"color": 0xFF0000
+		},
+		"pause":{
+			"word1": "Pausing",
+			"word2": "⏸️ - Paused",
+			"word3": "paused",
+			"color": 0x444444
+
+		}
+	}
+	word1 = terms[action]["word1"]
+	word2 = terms[action]["word2"]
+	word3 = terms[action]["word3"]
+	color = terms[action]["color"]
+	for message in messages:	
+		try:
+			messagecontents = message.embeds[0]
+			splitmessage = messagecontents.description.splitlines()
+			l1 = splitmessage[0]
+			l2 = splitmessage[1]
+			l3 = splitmessage[2]
+			l4 = splitmessage[3]
+			l5 = splitmessage[4]
+			l6 = splitmessage[5]
+			if l1 == f'__**{user.name}#{user.discriminator}**__':		
+				await infomsg.edit(content=f"{word1} {user.name}...")
+				embed = discord.Embed(color=color, description=l1)
+				addrow(embed, l2)
+				addrow(embed, l3)
+				addrow(embed, l4)
+				addrow(embed, l5)
+				
+				addfield(embed, "Invite Status", f'{word2} by {ctx.author.mention}')
+				embed.set_thumbnail(url=messagecontents.thumbnail.url)
+				await message.edit(embed=embed)			
+			else:
+				return
+		except IndexError:
+			await ctx.reply(f'{user.name} not found in {invitechannel.mention}')
+	await infomsg.edit(content=f"{user.name} successfully {word3}")	
 	
 
 class MdspCog(commands.Cog, name="MDSP"):
@@ -53,40 +117,38 @@ class MdspCog(commands.Cog, name="MDSP"):
 					await ctx.reply("Invalid User ID")
 		 
 		user = await self.bot.fetch_user(int(userid))
-		invitechannel = self.bot.get_channel(802245295291236442)
-		await ctx.reply(f'Adding "{user.name}" to {invitechannel.mention}...')
+		invitechannel = self.bot.get_channel(invitechannelid)
+		infomsg = await ctx.reply(f'Adding "{user.name}" to {invitechannel.mention}...')
 		try:
 			details = await mee6API.levels.get_user_details(userid)
 			mclevel = details["level"]
 			mcmessages = details["message_count"]
 		except TypeError:
-			mclevel = "Not enough"
-			mcmessages = "Not enough"
+			mclevel = "Not found, too low?"
+			mcmessages = "Not found, too low?"
 
-		embed = discord.Embed(color=embedcolor, description=f'__**{user.name}#{user.discriminator}**__')
+		embed = discord.Embed(color=0xffff00, description=f'__**{user.name}#{user.discriminator}**__')
 		embed.set_thumbnail(url=user.avatar_url)
 		addfield(embed, "Maincord Level", mclevel)
 		addfield(embed, "Maincord Messages", mcmessages)
 		addfield(embed, "Mention", user.mention)
 		addfield(embed, "User ID",  f'`{user.id}`')
-		
+		addfield(embed, "Invite Status",  f'None')
+		await infomsg.edit(content=f'Added "{user.name}" to {invitechannel.mention}')
 		await invitechannel.send(embed=embed)
 
 
 	@invite.command()
 	async def update(self, ctx):
-		invitechannel = self.bot.get_channel(802245295291236442)
-		messages = await invitechannel.history(limit=1).flatten() #TODO change limit when ready for actual thing happening time
+		invitechannel = self.bot.get_channel(invitechannelid)
+		messages = await invitechannel.history(limit=invitechannellimit).flatten() 
 		infomsg = await ctx.reply(f"Updating {invitechannel.mention}")
 		for message in messages:
 			try:
 				messagecontents = message.embeds[0]
-				splitmessage = messagecontents.description.splitlines()
-				l1 = splitmessage[0]
-				l2 = splitmessage[1]
-				l3 = splitmessage[2]
-				l4 = splitmessage[3]
-				l5 = splitmessage[4]
+				l1, l2, l3, l4, l5, l6 = listlines(messagecontents)
+				
+					
 				userid = ''.join(x for x in l5 if x.isdigit())
 				await infomsg.edit(content=f"Updating {invitechannel.mention}:\n {l1}")
 				try:
@@ -94,13 +156,14 @@ class MdspCog(commands.Cog, name="MDSP"):
 					mclevel = details["level"]
 					mcmessages = details["message_count"]
 				except TypeError:
-					mclevel = "Not enough"
-					mcmessages = "Not enough"
-				embed = discord.Embed(color=embedcolor, description=l1)
+					mclevel = "Not found, too low?"
+					mcmessages = "Not found, too low?"
+				embed = discord.Embed(color=0xffff00, description=l1)
 				addfield(embed, "Maincord Level", mclevel)
 				addfield(embed, "Maincord Messages", mcmessages)
 				addrow(embed, l4)
 				addrow(embed, l5)
+				addrow(embed, l6)
 				embed.set_thumbnail(url=messagecontents.thumbnail.url)
 				await message.edit(embed=embed)			
 			except IndexError:
@@ -108,8 +171,21 @@ class MdspCog(commands.Cog, name="MDSP"):
 		await infomsg.edit(content=f"Updating {invitechannel.mention}:\n Completed")		
 		
 			
+	@invite.command()
+	async def approve(self, ctx, userid):		
+		invitechannel = self.bot.get_channel(invitechannelid)
+		user = await self.bot.fetch_user(int(userid))		
+		messages = await invitechannel.history(limit=invitechannellimit).flatten()
+		infomsg = await ctx.reply(f"Searching for {user.name} in {invitechannel.mention}...")
+		await updateinvitestatus(ctx, infomsg, messages, user, invitechannel, "approve")
 
-
+	@invite.command()
+	async def deny(self, ctx, userid):		
+		invitechannel = self.bot.get_channel(invitechannelid)
+		user = await self.bot.fetch_user(int(userid))		
+		messages = await invitechannel.history(limit=invitechannellimit).flatten()
+		infomsg = await ctx.reply(f"Searching for {user.name} in {invitechannel.mention}...")
+		await updateinvitestatus(ctx, infomsg, messages, user, invitechannel, "deny")
 
 
 
