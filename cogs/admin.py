6f909f1e3ac84a88896ca950ec481e67662d95d7
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-03-03 16:18:17 -0800

    change perge and delete command permission requirements

--- cogs/admin.py
+++ cogs/admin.py
@@ -16,35 +16,42 @@ class AdminCog(commands.Cog, name="Admin"):
 		self.bot = bot
 
 	@commands.command()
+	@commands.has_guild_permissions(manage_messages=True)
 	async def purge(self, ctx):
-		if ctx.message.author.id != 545463550802395146:
-			await ctx.message.add_reaction(str('🔒'))
-			return
-		else:
-			args = ctx.message.content.split(" ")
-			if args[1]:
-				try:
-					amount = int(args[1])
-					await ctx.message.delete()				
-					await ctx.channel.purge(limit=amount)
-					embed = discord.Embed(color=embedcolor)
-					embed.add_field(name="Clear", value="cleared " + args[1] + " messages")
-					embed.set_footer(text=f"Request by {ctx.author}", icon_url= ctx.author.avatar_url)
-					await ctx.send(embed=embed)
-				except:
-					await ctx.reply("Error, most likely not a number")
+		args = ctx.message.content.split(" ")
+		if args[1]:
+			try:
+				amount = int(args[1])
+				await ctx.message.delete()				
+				await ctx.channel.purge(limit=amount)
+				embed = discord.Embed(color=embedcolor)
+				embed.add_field(name="Clear", value="cleared " + args[1] + " messages")
+				embed.set_footer(text=f"Request by {ctx.author}", icon_url= ctx.author.avatar_url)
+				await ctx.send(embed=embed)
+			except:
+				await ctx.reply("Error, most likely not a number")
 
 	@commands.command()
+	@commands.has_guild_permissions(manage_messages=True)
 	async def delete(self, ctx, messageid):
-		if ctx.message.author.id != 545463550802395146:
-			await ctx.message.add_reaction(str('❔'))
-			return
-		else:
+		message:discord.Message = await MessageConverter().convert(ctx, messageid)
+		if message.guild.id == ctx.guild.id:	
 			await ctx.message.delete()
-			message = await MessageConverter().convert(ctx, messageid)
 			await message.delete()
 
 
+	@commands.command()
+	@commands.has_guild_permissions(manage_nicknames=True)
+	async def nickname(self, ctx:commands.Context, *args):
+		args = list(args)
+		if len(ctx.message.mentions) == 1:
+			user = ctx.message.mentions[0]
+			args.pop(args.index(user.mention))
+		else:
+			user = await ctx.guild.fetch_member(args[0])
+			args.pop(args.index(user.id))
+		nick = ' '.join(args)
+		await user.edit(nick=nick)
 
 
 def setup(bot):
