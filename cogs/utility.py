a2b4b9c7e67efbbb8ca2b024ccc82ecb8267cc3c
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-05-16 20:23:21 -0700

    clean up cog, change method for adding roles

--- cogs/utility.py
+++ cogs/utility.py
@@ -115,28 +115,11 @@ class UtilityCog(commands.Cog, name="Utility"):
 			else:
 				ismobile = ""
 			statusicon = statuseemojis[str(status)]
-
-			def embedsec1(embed):
-				EmbedMaker.add_description_field(embed, "Is the owner?", isowner)
-				EmbedMaker.add_description_field(embed, "Is an admin?", isadmin)
-				EmbedMaker.add_blank_field(embed)
-				EmbedMaker.add_description_field(embed, "Status", f'{ismobile}{statusicon}{statusmsg}')
-				EmbedMaker.add_blank_field(embed)
-			def embedsec2(embed):
-				EmbedMaker.add_description_field(embed, "Join Date", joindate)
-			def embedsec3(embed):
-				EmbedMaker.add_description_field(embed, "Nickname", nickname)
 		else:
 			try:
 				user = await self.bot.fetch_user(int(userid))
 			except discord.errors.NotFound:
 				await ctx.reply("Invalid User ID!")
-				return
-			def embedsec1(embed):
-				return
-			def embedsec2(embed):
-				return
-			def embedsec3(embed):
 				return
 		flags = user.public_flags.all()
 		badgelist = {
@@ -166,14 +149,22 @@ class UtilityCog(commands.Cog, name="Utility"):
 		embed       = discord.Embed(color=color,title=username)
 		embed.set_footer(text=f"Request by {ctx.author}", icon_url= ctx.author.avatar_url)
 		embed.set_image(url=avatar)
-		EmbedMaker.add_description_field(embed, "Is a bot?", isbot)
-		embedsec1(embed)
+		if isguildmember:
+			EmbedMaker.add_description_field(embed, "Is a bot?", isbot)
+			EmbedMaker.add_description_field(embed, "Is the owner?", isowner)
+			EmbedMaker.add_description_field(embed, "Is an admin?", isadmin)
+			EmbedMaker.add_blank_field(embed)
+			EmbedMaker.add_description_field(embed, "Status", f'{ismobile}{statusicon}{statusmsg}')
+			EmbedMaker.add_blank_field(embed)
+
 		EmbedMaker.add_description_field(embed, "Mention", mention)
-		embedsec3(embed)
+		if isguildmember:
+			EmbedMaker.add_description_field(embed, "Nickname", nickname)
 		EmbedMaker.add_description_field(embed, "User ID", f'`{user.id}`')
 		EmbedMaker.add_blank_field(embed)
 		EmbedMaker.add_description_field(embed, "Account Creation Date", createdate)
-		embedsec2(embed)
+		if isguildmember:
+			EmbedMaker.add_description_field(embed, "Join Date", joindate)
 		EmbedMaker.add_blank_field(embed)
 		if str(badgeicons) != "[]":
 			EmbedMaker.add_description_field(embed, "Profile Badges", badgestr)
@@ -188,14 +179,14 @@ class UtilityCog(commands.Cog, name="Utility"):
 		embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
 		await suggestion_channel.send(embed=embed)
 		await ctx.reply("Suggestion added")
-		
-		@commands.command()
-		async def selfmute(self, ctx, duration: int):
-			member = ctx.author.id
-			role = discord.utils.get(member.server.roles, name='Muted')
-			await bot.add_roles(member, role)
-			await asyncio.sleep(duration)
-			await self.bot.remove_roles(member, role)
+
+	@commands.command()
+	async def selfmute(self, ctx, duration: int):
+		member = ctx.author.id
+		muted_role = discord.utils.get(member.server.roles, name='Muted')
+		await ctx.author.add_roles(muted_role, reason=f"Requested self-mute for {duration}")
+		await asyncio.sleep(duration)
+		await ctx.author.remove_roles(muted_role, reason=f"Requested self-mute for {duration} has expired")
 
 
 	@commands.command(aliases=["online", "areyouthere"])
