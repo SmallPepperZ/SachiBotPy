a54ae5bb852d2739346a51c7b84a8bd0cc330422
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-06-14 09:03:10 -0700

    remove useless class in dbfunctions

--- customfunctions/funcs/dbfunctions.py
+++ customfunctions/funcs/dbfunctions.py
@@ -1,26 +1,27 @@
-class DatabaseFromDict():
-	def make_placeholder(table:str, dict:dict, *, operation:str='insert'):
-		"""Returns an SQLite string from the input dictionary
 
-		Parameters
-		----------
-		table: str
-			Which table of the database to insert to
-		dict : dict
-			The dictionary with the data, uses the keys as columns
+def make_placeholder(table:str, dictionary:dict):
+	"""Returns an SQLite string from the input dictionary
 
-		Keyword Parameters
-		------------------
-		operation:str = insert
-			Which operation to use, by default, insert
+	Parameters
+	----------
+	table: str
+		Which table of the database to insert to
+	dictionary : dict
+		The dictionary with the data, uses the keys as columns
 
-		Returns
-		-------
-		str
-			An SQLite command to use with `dbcon.executemany`
-		"""
-		columns = tuple(dict.keys())
-		valueplaceholder = '?'
-		for item in range(len(columns)-1): valueplaceholder += ', ?'
-		sql  = f"insert or replace into {table} {columns} values ({valueplaceholder})"
-		return sql
+	Keyword Parameters
+	------------------
+	operation:str = insert
+		Which operation to use, by default, insert
+
+	Returns
+	-------
+	str
+		An SQLite command to use with `dbcon.executemany`
+	"""
+	columns = tuple(dictionary.keys())
+	valueplaceholder = '?'
+	for item in range(len(columns)-1):
+		valueplaceholder += ', ?'
+	sql  = f"INSERT or REPLACE into {table} {columns} values ({valueplaceholder})"
+	return sql
