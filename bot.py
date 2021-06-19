dfb3793b319decef5260bc31f082b1c375a14289
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-06-19 14:48:37 -0700

    move uncaght error handling to own file

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@
 
 import time
 import json
-import traceback
 import discord
 
-import requests
 
 
 from discord.ext import commands
@@ -13,7 +11,7 @@ from discord.ext.commands import CommandNotFound, errors
 from discord_slash import SlashCommand
 
 from customfunctions.funcs import handling #pylint:disable=unused-import
-from customfunctions import config, set_config
+from customfunctions import config
 from customfunctions import CustomChecks, ErrorHandling
 from customfunctions import master_logger
 # endregion
@@ -70,7 +68,10 @@ bot.coglist = [	'cogs.owner',
 
 if __name__ == '__main__':
 	for extension in bot.coglist:
-		bot.load_extension(extension)
+		try:
+			bot.load_extension(extension)
+		except:
+			pass
 # endregion
 
 # region Logger Stuff
@@ -106,8 +107,8 @@ async def get_error(ctx, error:object):
 		errors.MissingPermissions       : lambda: ctx.message.add_reaction(str('🔐')),
 		errors.BotMissingPermissions    : lambda: ctx.reply("I do not have the requisite permissions"),
 		errors.MissingRole              : lambda: ctx.message.add_reaction(str('🔐')),
-		errors.MissingRequiredArgument  : lambda: ctx.reply(f"Missing required argument!\nUsage:`{ctx.command.signature}`", delete_after=30),
-		errors.BadArgument              : lambda: ctx.reply(f"Missing required argument!\nUsage:`{ctx.command.signature}`", delete_after=30),
+		errors.MissingRequiredArgument  : lambda: ErrorHandling.invalid_invocation(ctx,error),
+		errors.BadArgument              : lambda: ErrorHandling.invalid_invocation(ctx,error),
 		errors.NoPrivateMessage         : lambda: ctx.message.add_reaction(str('<:ServerOnlyCommand:803789780793950268>')),
 		discord.errors.Forbidden        : lambda: ctx.reply("I do not have the requisite permissions"),
 		CustomChecks.IncorrectGuild     : lambda: ctx.reply(content="This command does not work in this server.", delete_after=10),
@@ -117,74 +118,8 @@ async def get_error(ctx, error:object):
 	error_type = type(error.original) if isinstance(error, errors.CommandInvokeError) else type(error) # Get the type of the error
 	if error_type in error_handling.keys(): # check if the error is in the dictionary dictionary and if so, call the handling function
 		await error_handling[error_type]()
-	else: # if the error isn't handled, handle it with the ungaught error handler
-		await uncaught_error(ctx, error)
-
-async def uncaught_error(ctx, error):
-	error_str = str(error).replace(personal_info, '')
-	# Send user a message
-	await ctx.message.add_reaction('<:CommandError:804193351758381086>')
-	await ctx.reply("Error:\n```"+error_str+"```\nSmallPepperZ will be informed", delete_after=60)
-
-	# Get traceback info
-
-	lines = traceback.format_exception(type(error), error, error.__traceback__)
-	traceback_text = ''.join(lines)
-
-	# Github gist configuration
-	errornum = int(config("errornum"))+1
-	set_config("errornum", str(errornum))
-
-	traceback_text = traceback_text.replace(personal_info, '')
-
-	apiurl = "https://api.github.com/gists"
-	gist_id = config("githubgist")
-	githubtoken = config('githubtoken')
-
-	payload = {"description": "SachiBot Errors - A gist full of errors for my bot",
-			   "public": False,
-			   "files": {
-				   f"SachiBotPyError {errornum:02d}.log": {
-					   "content": f'Error - {error} \n\n\n {traceback_text}'
-					   }
-					}
-				}
-	# Upload to github gist
-	requests.patch(f'{apiurl}/{gist_id}',
-				   headers={'Authorization': f'token {githubtoken}'},
-				   params={'scope': 'gist'},
-				   data=json.dumps(payload))
-	# Build and send embed for error channel
-	channel = bot.get_channel(errorchannel)
-	embed1 = discord.Embed(
-		title=f"Error {errornum:02d}", color=embedcolor)
-	embed1.add_field(name="Message Url:",
-						value=ctx.message.jump_url, inline='false')
-	embed1.add_field(
-		name="Message:", value=ctx.message.clean_content, inline='true')
-	embed1.add_field(
-		name="Author:", value=ctx.message.author.mention, inline='true')
-	embed1.add_field(name="\u200B", value='\u200B', inline='true')
-	# Check if it was in a guild
-	try:
-		guildname = ctx.guild.name
-		channelname = ctx.channel.name
-	except:
-		guildname = "DM"
-		channelname = ctx.author.id
-	embed1.add_field(name="Guild:", value=guildname, inline='true')
-	embed1.add_field(name="Channel:", value=channelname, inline='true')
-	embed1.add_field(name="\u200B", value='\u200B', inline='true')
-	embed1.add_field(name="Error:", value=f'```{error}```', inline='false')
-	embed1.add_field(
-		name="Traceback:", value=f'Traceback Gist - '
-									f'[SachiBotPyError {errornum:02d}.log](https://gist.github.com/SmallPepperZ/{gist_id}#file-sachibotpyerror-{errornum:02d}-log'
-									f' \"Github Gist #{errornum:02d}\") ', inline='false')
-	await channel.send(embed=embed1)
-
-	ghost_ping = await channel.send('<@!545463550802395146>')
-	await ghost_ping.delete()
-
+	else: # if the error isn't handled, handle it with the uncaught error handler
+		await ErrorHandling.uncaught_error(ctx, error, bot)
 
 
 # endregion
