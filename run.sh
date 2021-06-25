2dddb39f1c862c4b49d4ee10c5dd71fb41919d26
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-06-25 16:04:25 -0700

    check if session exists

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if ! which tmux >/dev/null; then
+if ! which tmux >/dev/null
+then
 	echo "tmux is not installed"
 	exit 0
-else
+elif ! tmux has-session -t SachiBot &> /dev/null
+then
 	cd `dirname $0`
 	source ./venv/bin/activate
 	tmux new-session -d -s SachiBot python3 "./bot.py"
+else
+	exit 1
 fi
