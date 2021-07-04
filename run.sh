9392f014ea783a913c873a5593ac4ee13e03014d
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-07-04 15:26:00 -0700

    actually redirect stdout and sterr

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ then
 	echo "1" > storage/disabled
 	cd `dirname $0`
 	source ./venv/bin/activate
-	/usr/local/bin/tmux new-session -d -s SachiBot python3 "./bot.py" &> out.log
+	/usr/local/bin/tmux new-session -d -s SachiBot python3 "./bot.py" &> logs/SachiBot.info.log
 else
 	echo "3.3" >> /Users/zachy/.tmp/test
 	exit 0
