d083905b277bd59a3e67450689220b46426acea3
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-07-12 18:32:22 -0700

    add some minecraft commands

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from customfunctions import config
+from customfunctions import config, MinecraftApi
 
 
 embedcolor = config("embedcolor")
@@ -13,10 +13,35 @@ class MinecraftCog(commands.Cog, name="Minecraft"):
 	async def minecraft(self, ctx):
 		if ctx.invoked_subcommand is None:
 			subcommands = [
-				f'**{cmd.name}:** {cmd.description}' for cmd in ctx.command.commands]
+				f'**`{cmd.name}`:** {cmd.help}' for cmd in ctx.command.commands]
 			embed = discord.Embed(color=embedcolor, title="Minecraft Subcommands:",
 								  description="\n\n".join(list(map(str, subcommands))))
 			await ctx.reply(embed=embed)
+
+	@minecraft.command()
+	async def skin(self, ctx, username:str):
+		"""
+		Shows a 3D render of a user's skin
+		"""
+		try:
+			user = MinecraftApi.MinecraftUser(username)
+		except ValueError:
+			await ctx.reply("Invalid username")
+		embed = discord.Embed(title=user.name,color=embedcolor)
+		embed.set_footer(text="Avatars provided by Crafatar.com")
+		embed.set_image(url=user.skin("body_render"))
+		await ctx.reply(embed=embed)
+
+	@minecraft.command()
+	async def uuid(self, ctx, username:str):
+		"""
+		Gets the user's uuid from their username
+		"""
+		try:
+			user = MinecraftApi.MinecraftUser(username)
+		except ValueError:
+			await ctx.reply("Invalid username")
+		await ctx.reply(f'`{user.uuid}`')
 
 def setup(bot):
 	bot.add_cog(MinecraftCog(bot))
